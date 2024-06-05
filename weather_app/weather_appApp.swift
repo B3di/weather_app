5c45a77f3d3d7dad09592eb8f3c20f79a2e1268c
b3di <mrb3d1@gmail.com>
2024-06-05 18:30:15 +0200

    Initial Commit

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by Admin on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
